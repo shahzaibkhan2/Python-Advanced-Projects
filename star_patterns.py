b6e642bc0a021eed808e820368dfeeb492932a17
Software Engineer <122774230+shahzaibkhan2@users.noreply.github.com>
2023-04-18 08:10:57 -0700

    Star Patterns with Easy Code
    Easy Code for Star Patterns

---
+++ star_patterns.py
@@ -0,0 +1,74 @@
+# 1. Increasing star pattern
+# user = int(input('enter number of rows: '))
+# for i in range(1,user+1):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+    
+
+# 2.Decreasing star pattern
+# user1 = int(input('enter number of rows: '))
+# for i in range(0,user1):     # zero(0) makes it easier
+#     for j in range(0,user1-i):
+#         print('*',end='')
+#     print()
+
+
+
+# k = 1
+# user = int(input('enter number of rows: '))
+# for i in range(1,user+1):
+#     for j in range(1,i+1):
+#         print(k,end='')
+#     print()
+
+
+# k = 1
+# user = int(input('enter number of rows: '))
+# for i in range(1,user+1):
+#     for j in range(1,i+1):
+#         print(k,end='')
+#         k+=1
+#     print()
+
+
+
+# no_rows = int(input('enter number of rows: '))
+# 1. 
+# for row in range(no_rows,0,-1):
+#     for column in range(1,row+1):
+#         print('*',end=' ')
+        
+#     print()
+# # 2.
+# for row in range(1,no_rows+1):
+#     for column in range(1,row+1):
+#         print('*',end=' ')
+#     print()
+    
+# for row in range(no_rows,0,-1):
+#     for column in range(1,row):  # just eliminated one colu
+#         print('*',end=' ')
+        
+#     print()
+
+# 3. you can edit in whatever shape you want   
+# for row in range(1,no_rows+1):
+#     for column in range(1,row+1):
+#         print('{}{}'.format(row,column),end=' ')
+#     print()
+    
+    
+    
+# def addlist(a):
+#     sum1 = 0
+#     for i in a:
+#         sum1 = sum1+i
+#     print('sum of list is',sum1)
+    
+# b = [1,2,34,5,6,8,5]
+# obj1 = addlist(b)
+
+
+
+    
