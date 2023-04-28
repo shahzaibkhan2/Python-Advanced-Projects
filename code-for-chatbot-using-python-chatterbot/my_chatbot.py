81ffe4724d63ee0cbca75578a5cba97f601f8d5c
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-04-28 06:14:40 -0700

    Code for ChatBot using Python Chatterbot
    Code for ChatBot using Python Chatterbot

--- code-for-chatbot-using-python-chatterbot/my_chatbot.py
+++ code-for-chatbot-using-python-chatterbot/my_chatbot.py
@@ -1,1 +1,22 @@
+# Importing necessary modules
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
+# Creating a chatbot instance
+chatbot = ChatBot('MyChatBot')
+
+# Creating a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Training the chatbot which is based on the english corpus
+trainer.train('chatterbot.corpus.english')
+
+# Starting the chatbot
+print('Type "quit" to exit')
+while True:
+    user_input = input('You: ')
+    if user_input.lower() == 'quit':
+        break
+    else:
+        bot_response = chatbot.get_response(user_input)
+        print('Bot:', bot_response)
