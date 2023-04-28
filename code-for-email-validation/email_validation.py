fc96697b6c04cbd69f90e5ef90390a915a0c2055
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-04-28 06:23:30 -0700

    Code for Email Validation
    Code for Email Validation

--- code-for-email-validation/email_validation.py
+++ code-for-email-validation/email_validation.py
@@ -1,1 +1,17 @@
+import re
 
+# Defining a function to check email validity
+def is_valid_email(email):
+    """Checks if an email address is valid"""
+    # Regular expression for email validation
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(pattern, email, re.IGNORECASE):
+        return True
+    else:
+        return False
+
+# Examples for usage
+if is_valid_email('shahzaib11@gmail.com'):
+    print('Valid email')
+else:
+    print('Invalid email')
