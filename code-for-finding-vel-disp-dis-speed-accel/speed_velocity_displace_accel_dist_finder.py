30b54bfe1d1268354a6dcd128715ad932900476b
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-01 07:42:41 -0700

    Code for Finding vel,speed,dist,disp,accel
    Code for Finding vel,speed,dist,disp,accel

--- code-for-finding-vel-disp-dis-speed-accel/speed_velocity_displace_accel_dist_finder.py
+++ code-for-finding-vel-disp-dis-speed-accel/speed_velocity_displace_accel_dist_finder.py
@@ -1,1 +1,28 @@
+# Define initial and final conditions to find the resultant
+initial_position = 0  # meters
+final_position = 100  # meters
+initial_velocity = 0  # meters/second
+final_velocity = 50  # meters/second
+time = 10  # seconds
 
+# Calculate displacement
+displacement = final_position - initial_position
+
+# Calculate distance
+distance = abs(displacement)
+
+# Calculate average velocity
+average_velocity = displacement / time
+
+# Calculate acceleration
+acceleration = (final_velocity - initial_velocity) / time
+
+# Calculate speed
+speed = distance / time
+
+# Print results
+print("Displacement:", displacement, "meters")
+print("Distance:", distance, "meters")
+print("Average Velocity:", average_velocity, "meters/second")
+print("Acceleration:", acceleration, "meters/second^2")
+print("Speed:", speed, "meters/second")
