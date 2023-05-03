c05279b2253aa8687c089bc7c70709a5d38dfc89
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-03 07:32:10 -0700

    Code to find stock prices
    Code to find stock prices

--- code-for-stock-prices-finder/stock_prices_finder.py
+++ code-for-stock-prices-finder/stock_prices_finder.py
@@ -1,1 +1,14 @@
+import alpha_vantage
+from alpha_vantage.timeseries import TimeSeries
 
+# Replace YOUR_API_KEY with your Alpha Vantage API key
+ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+
+# Replace SYMBOL with the stock symbol you want to retrieve (e.g. MS for Microsoft)
+symbol = 'SYMBOL'
+
+# Retrieve the daily stock prices for the symbol you want
+data, meta_data = ts.get_daily(symbol=symbol)
+
+# Print the last 5 rows of the data
+print(data.tail())
