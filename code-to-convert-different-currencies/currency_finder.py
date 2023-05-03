fc543bfcab677a88c7025f458a19df38c7607d32
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-03 07:54:28 -0700

    Code to convert different currencies
    Code to convert different currencies

--- code-to-convert-different-currencies/currency_finder.py
+++ code-to-convert-different-currencies/currency_finder.py
@@ -1,1 +1,15 @@
+# import forex_python module
+from forex_python.converter import CurrencyRates
 
+# Create a CurrencyRates object
+c = CurrencyRates()
+
+# Convert 200 USD to EUR
+usd_amount = 200
+eur_amount = c.convert('USD', 'EUR', usd_amount)
+print(f"{usd_amount} USD = {eur_amount} EUR")
+
+# Convert 1000 JPY to USD
+jpy_amount = 1000
+usd_amount = c.convert('JPY', 'USD', jpy_amount)
+print(f"{jpy_amount} JPY = {usd_amount} USD")
