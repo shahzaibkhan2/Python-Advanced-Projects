04b396879a9251f4ba100c3b1922b08befd27c1f
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-04 07:28:48 -0700

    Code to find pressure volume temp entropy nmoles
    Code to find pressure volume temp entropy nmoles

--- code-to-find_pressure_vol_temp_entropy_nmoles/pressure_volume_temp_entropy_nmoles_finder.py
+++ code-to-find_pressure_vol_temp_entropy_nmoles/pressure_volume_temp_entropy_nmoles_finder.py
@@ -1,1 +1,29 @@
+# Import the necessary constants from the scipy.constants module
+from scipy.constants import R
 
+# Define the variables
+P = 1.0   # Pressure (in atm)
+V = 22.4  # Volume (in L)
+T = 273.0 # Temperature (in K)
+n = 1.0   # Number of moles
+
+# Calculate the pressure using the ideal gas law
+P = (n * R * T) / V
+
+# Calculate the volume using the ideal gas law
+V = (n * R * T) / P
+
+# Calculate the temperature using the ideal gas law
+T = (P * V) / (n * R)
+
+# Calculate the entropy
+deltaS = 50.0  # Change in entropy (in J/K)
+qrev = deltaS * T
+S = qrev / T
+
+# Print the results
+print("Pressure = ", P, "atm")
+print("Volume = ", V, "L")
+print("Temperature = ", T, "K")
+print("Entropy = ", S, "J/K")
+print("Number of moles = ", n)
