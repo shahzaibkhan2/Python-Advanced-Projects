83d68d435c74e98309606fff8c60c54b99e65428
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-05 08:23:12 -0700

    Code to store input from user to JSON
    Code to store input from user to JSON

--- code-to-store-input-in-json/store_input_from_user_to_json.py
+++ code-to-store-input-in-json/store_input_from_user_to_json.py
@@ -1,1 +1,21 @@
+import json
 
+# Prompt the user for input
+name = input("What is your name? ")
+age = input("What is your age? ")
+phone = input("What is your phone number? ")
+email = input("What is your email? ")
+
+# Create a dictionary to store the user's information
+user_info = {
+    "Name": name,
+    "Age": age,
+    "Phone": phone,
+    "Email": email
+}
+
+# Save the user's information to a JSON file
+with open("user_info.json", "w") as file:
+    json.dump(user_info, file)
+
+print("User info saved to user_info.json")
