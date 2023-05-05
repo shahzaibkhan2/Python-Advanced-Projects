90b751c9711a544b78a80e1ea2b27213daa6d190
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-05 08:53:05 -0700

    Code to scrap data from website
    Code to scrap data from website using beautiful soup

--- code-to-scrap-data-from-website-using-beautiful-soup/web_scrapping_using_beautifulsoup.py
+++ code-to-scrap-data-from-website-using-beautiful-soup/web_scrapping_using_beautifulsoup.py
@@ -1,1 +1,19 @@
+# Import necessary modules
+import requests
+from bs4 import BeautifulSoup
 
+# Define the URL to scrape data from website
+url = 'https://www.example.com'
+
+# Make a GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content using Beautiful Soup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find all the links on the page
+links = soup.find_all('a')
+
+# Print out the text of each link
+for link in links:
+    print(link.text)
