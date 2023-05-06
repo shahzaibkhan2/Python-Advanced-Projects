e4374e8856400b9d8cf23230abe9127f9308d440
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-06 08:14:42 -0700

    Code to convert centi,mili,micro,nano,femto,atto
    Code to convert centi,mili,micro,nano,femto,atto

--- code-for-centi-mili-micro-nano-femto-atto-converter/centi_mili_micro_nano_femto_atto_converter.py
+++ code-for-centi-mili-micro-nano-femto-atto-converter/centi_mili_micro_nano_femto_atto_converter.py
@@ -1,1 +1,39 @@
+# Define a function
+def convert(value, unit):
+    """Converts a value from one unit to another unit"""
+    conversions = {
+        "cm": 0.01,  # centimeters to meters
+        "mm": 0.001,  # millimeters to meters
+        "µm": 0.000001,  # micrometers to meters
+        "nm": 0.000000001,  # nanometers to meters
+        "pm": 0.000000000001,  # picometers to meters
+        "fm": 0.000000000000001,  # femtometers to meters
+        "am": 0.000000000000000001  # attometers to meters
+    }
 
+    # Convert value to meters
+    meters = value * conversions[unit]
+
+    # Convert meters to other units
+    centimeters = meters / conversions["cm"]
+    millimeters = meters / conversions["mm"]
+    micrometers = meters / conversions["µm"]
+    nanometers = meters / conversions["nm"]
+    picometers = meters / conversions["pm"]
+    femtometers = meters / conversions["fm"]
+    attometers = meters / conversions["am"]
+
+    # Return a dictionary of conversion results
+    return {
+        "cm": centimeters,
+        "mm": millimeters,
+        "µm": micrometers,
+        "nm": nanometers,
+        "pm": picometers,
+        "fm": femtometers,
+        "am": attometers
+    }
+
+
+convert(1, "cm")
+{'cm': 1.0, 'mm': 10.0, 'µm': 10000.0, 'nm': 10000000.0, 'pm': 10000000000.0, 'fm': 1e+16, 'am': 1e+19}
