48b607afc7a2a8d24ed87f44d0c8765531847d4b
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-13 09:02:30 -0700

    Code for Debugging bugs
    Code for Debugging bugs using Python

--- code-for-debugging-bugs/debugging.py
+++ code-for-debugging-bugs/debugging.py
@@ -1,1 +1,30 @@
+# The code which is need to be debugged
+def calculate_average(numbers):
+    total = 0
+    count = 0
+    for num in numbers:
+        total += num
+        count += 1
+    average = total / count
+    return average
 
+numbers = [5, 6, 3, 4, 8]
+test = calculate_average(numbers)
+print("The average is: " + test)
+
+
+
+
+# Debugged code
+def calculate_average(numbers):
+    total = 0
+    count = 0
+    for num in numbers:
+        total += num
+        count += 1
+    average = total / count
+    return average
+
+numbers = [5, 6, 3, 4, 8]
+test = calculate_average(numbers)
+print("The average is: " + str(test))
