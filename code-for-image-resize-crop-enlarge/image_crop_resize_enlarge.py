116bdc78b0956fad0a912947e82868aa7897d9d3
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-14 11:35:09 -0700

    Code for Image resize, enlarge and crop
    Code for Image resize, enlarge and crop using Python

--- code-for-image-resize-crop-enlarge/image_crop_resize_enlarge.py
+++ code-for-image-resize-crop-enlarge/image_crop_resize_enlarge.py
@@ -1,1 +1,24 @@
+# Import PIL module
+from PIL import Image
 
+# Load the image
+image = Image.open('sample.jpg')
+
+# Resize the image
+resized_image = image.resize((500, 500))
+
+# Crop the image
+width, height = image.size
+left = (width - 500) / 2
+top = (height - 500) / 2
+right = (width + 500) / 2
+bottom = (height + 500) / 2
+cropped_image = image.crop((left, top, right, bottom))
+
+# Enlarge the image
+enlarged_image = image.resize((width * 2, height * 2))
+
+# Save the images
+resized_image.save('resized.jpg')
+cropped_image.save('cropped.jpg')
+enlarged_image.save('enlarged.jpg')
