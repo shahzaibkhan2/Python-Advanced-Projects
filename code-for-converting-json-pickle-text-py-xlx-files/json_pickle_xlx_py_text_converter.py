a3f7cb8a9f34cfababc24bb041125ce1245d57fd
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-14 07:56:34 -0700

    Code for converting json,pickle,text,xlx,py files
    Code for converting json,pickle,text,xlx,py files using Python and its modules like json, pickle and openpyxl

--- code-for-converting-json-pickle-text-py-xlx-files/json_pickle_xlx_py_text_converter.py
+++ code-for-converting-json-pickle-text-py-xlx-files/json_pickle_xlx_py_text_converter.py
@@ -1,1 +1,49 @@
+# Importing necessary modules
+import json
+import pickle
+import openpyxl
 
+# Convert JSON to pickle
+def json_to_pickle(json_data, pickle_file):
+    with open(pickle_file, 'wb') as f:
+        pickle.dump(json_data, f)
+
+# Convert pickle to JSON
+def pickle_to_json(pickle_file, json_file):
+    with open(pickle_file, 'rb') as f:
+        data = pickle.load(f)
+        with open(json_file, 'w') as outfile:
+            json.dump(data, outfile)
+
+# Convert JSON to XLSX
+def json_to_xlsx(json_data, xlsx_file):
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    for row_num, row_data in enumerate(json_data):
+        for col_num, col_data in enumerate(row_data.values()):
+            worksheet.cell(row=row_num+1, column=col_num+1, value=col_data)
+    workbook.save(xlsx_file)
+
+# Convert XLSX to text
+def xlsx_to_text(xlsx_file, text_file):
+    workbook = openpyxl.load_workbook(xlsx_file)
+    worksheet = workbook.active
+    with open(text_file, 'w') as f:
+        for row in worksheet.iter_rows(values_only=True):
+            f.write('\t'.join(str(cell) for cell in row) + '\n')
+
+# Convert text to Python file
+def text_to_pyfile(text_file, py_file):
+    with open(text_file, 'r') as f:
+        text_data = f.read()
+        with open(py_file, 'w') as outfile:
+            outfile.write('data = """' + text_data + '"""')
+
+# Example for usage:
+json_data = {'random_key1': 'random_value1', 'random_key2': 'random_value2', 'random_key3': 'random_value3'}
+
+json_to_pickle(json_data, 'main_data.pickle')
+pickle_to_json('data.pickle', 'main_data.json')
+json_to_xlsx([json_data], 'main_data.xlsx')
+xlsx_to_text('data.xlsx', 'main_data.txt')
+text_to_pyfile('data.txt', 'main_data.py')
