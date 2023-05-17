512dd9ddb77b91d189a3a2d5dcb2eaedcffe9c04
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-16 21:49:36 -0700

    Code for English spelling check
    Code for English spelling check using Python

--- code-for-english-spelling-check/spelling_check_english.py
+++ code-for-english-spelling-check/spelling_check_english.py
@@ -1,1 +1,24 @@
+# Import pyasell module which is a Python's Library
+import pyaspell
 
+# Function to correct spellings
+def correct_spelling(word):
+    checker = pyaspell.Speller()
+    suggestions = checker.suggest(word)
+    if suggestions:
+        return suggestions[0]
+    else:
+        return word
+
+# Function for spelling check
+def spell_check_text(text):
+    words = text.split()
+    corrected_text = []
+    for word in words:
+        corrected_text.append(correct_spelling(word))
+    return ' '.join(corrected_text)
+
+# Example for usage
+sample_text = "I am a Python develepar andd data analistt."
+corrected_text = spell_check_text(sample_text)
+print(corrected_text)
