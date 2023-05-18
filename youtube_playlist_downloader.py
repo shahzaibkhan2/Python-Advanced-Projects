5f5afd3a47353bd9ffe0e0f45001034ec4547f0e
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-17 22:35:03 -0700

    delete

--- youtube_playlist_downloader.py
+++
@@ -1,8 +0,0 @@
-from pytube import Playlist
-
-yt_playlist = Playlist("https://www.youtube.com/watch?v=6Zbhvaac68Y&list=PLlL_rsmcRMZfDx2VycMXNYc9bERDgYvPi")
-
-print(f'downloading: {yt_playlist.title}')
-
-for video in yt_playlist.videos:
-    video.streams.first().download()
