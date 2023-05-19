a01377515349f996364ceee2e8be10e7dc22585e
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-18 21:24:09 -0700

    Code for single video youtube downloader
    Code for single video youtube downloader using Python

--- code-for-single-video-youtube-downloader/youtube_single_video_downloader.py
+++ code-for-single-video-youtube-downloader/youtube_single_video_downloader.py
@@ -1,1 +1,17 @@
+# ------ For single video only ------
+from pytube import YouTube
 
+link = "https://www.youtube.com/watch?v=bKDhmENcKto"
+youtube_title = YouTube(link)
+# print(youtube_title.title)
+# print(youtube_title.thumbnail_url)
+videos = youtube_title.streams.all()
+# videos_2 = youtube_title.streams.filter(only_video=True) # for videos only
+# videos_1 = youtube_title.streams.filter(only_audio=True) # for audio only
+vid = list(enumerate(videos))
+for i in vid:
+    print(i)
+print()
+stream_to_download = int(input('Enter your stream: '))
+videos[stream_to_download].download()
+print('Successfully downloaded!')
