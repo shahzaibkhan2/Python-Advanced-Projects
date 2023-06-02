de73eb22bdae62528d3975711440e89f7f85041e
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-02 09:21:05 -0700

    Code for all types of formats
    Code for all types of formats using Python

--- code-for-py-all-types-of-formats/py_formats.py
+++ code-for-py-all-types-of-formats/py_formats.py
@@ -1,1 +1,16 @@
+# 1.format_symbols_and_their_work
+print(f"addition of %d and %d = %d"%(2,3,2+3))
 
+# with references
+a =int(input("enter your first number: "))
+b = int(input("enter your second number: "))
+c = a+b
+print(f"addition of %d and %d = %d"%(a,b,c))
+print(f"addition of",a,"with",b,"=",c)  # same answer but with different format
+
+# very important concept
+dict1 = {'name':'shahzaib','age':25}
+print(f"%(name)s and %(age)i"%dict1)
+
+# 2. another format function
+print("name of the student is {0[name]:2s} and his/her age is {0[age]:2d}".format({'name':'shahzaib','age':25}))
