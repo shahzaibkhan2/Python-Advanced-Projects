e71dadc5a7f8eeb9acec9dd8f6be0891b48b782b
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-02 09:28:59 -0700

    Code for statistical relations
    Code for statistical relations using Python

--- code-for-statistics-relations/stats_relations.py
+++ code-for-statistics-relations/stats_relations.py
@@ -1,1 +1,37 @@
+# Import numpy
+import numpy as np
 
+# Function to calculate statistics
+def calculate_statistics(data):
+    # Standard Deviation
+    std_dev = np.std(data)
+
+    # Variance
+    variance = np.var(data)
+
+    # Maximum
+    maximum = np.max(data)
+
+    # Minimum
+    minimum = np.min(data)
+
+    # Interquartile Range
+    q75, q25 = np.percentile(data, [75 ,25])
+    iqr = q75 - q25
+
+    # Percentiles
+    percentiles = np.percentile(data, [10, 25, 50, 75, 90])
+
+    return std_dev, variance, maximum, minimum, iqr, percentiles
+
+# Example for usage
+dataset = [5, 11, 12, 10, 14, 16, 21, 22, 24, 31]
+
+std_dev, variance, maximum, minimum, iqr, percentiles = calculate_statistics(dataset)
+
+print("Standard Deviation:", std_dev)
+print("Variance:", variance)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
+print("Interquartile Range:", iqr)
+print("Percentiles:", percentiles)
