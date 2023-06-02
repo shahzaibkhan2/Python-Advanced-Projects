189e730e157aca15d250a155457e362f1e0e0e2d
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-01 20:48:05 -0700

    Code for calculator
    Code for calculator using Python and Tkinter

--- code-for-calculator/calculator.py
+++ code-for-calculator/calculator.py
@@ -1,1 +1,106 @@
+from ast import Lambda, operator
+from tkinter import *
 
+def get_digit(digit):
+    
+    current = result_label["text"]
+    new = current + str(digit)
+    result_label.config(text=new)
+    
+def clear():
+    result_label.config(text="")
+    
+def get_operator(op):
+    global first_number, operator
+    
+    first_number = int(result_label["text"])
+    operator = op
+    result_label.config(text="")
+
+def get_result():
+    global first_number,second_number,operator
+    second_number = result_label["text"]
+    
+    if operator == "+":
+        result_label.config(text=str(first_number+second_number))
+        
+    elif operator=="-":
+        result_label.config(text=str(first_number-second_number))
+        
+    elif operator=="*":
+        result_label.config(text=str(first_number * second_number))
+        
+    else:
+        if second_number == 0:
+            result_label.config(text="Error")
+            
+        else:
+            result_label.config(text=str(first_number / second_number))
+    
+        
+    
+
+root = Tk()
+root.title("Calculator")
+root.geometry("200x306")
+root.configure(background="black")
+root.resizable(0,0)
+
+result_label = Label(root,text="",bg="black",fg="white")
+result_label.grid(row=0,column=0,columnspan=20,pady=(50,25),sticky=W)
+result_label.config(font=("verdana",30,"bold"))
+
+btn_7 = Button(root,text="7",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(7))
+btn_7.grid(row=1,column=0)
+
+btn_8 = Button(root,text="8",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(8))
+btn_8.grid(row=1,column=1)
+
+btn_9 = Button(root,text="9",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(9))
+btn_9.grid(row=1,column=2)
+
+btn_plus = Button(root,text="+",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command=lambda :get_operator("+"))
+
+btn_plus.grid(row=1,column=3)
+
+btn_4 = Button(root,text="4",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(4))
+btn_4.grid(row=2,column=0)
+
+btn_5 = Button(root,text="5",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(5))
+btn_5.grid(row=2,column=1)
+
+btn_6 = Button(root,text="6",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(6))
+btn_6.grid(row=2,column=2)
+
+btn_minus = Button(root,text="-",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command=lambda :get_operator("-"))
+btn_minus.grid(row=2,column=3)
+
+btn_1 = Button(root,text="1",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(1))
+btn_1.grid(row=3,column=0)
+
+btn_2 = Button(root,text="2",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(2))
+btn_2.grid(row=3,column=1)
+
+btn_3 = Button(root,text="3",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(3))
+btn_3.grid(row=3,column=2)
+
+btn_mul = Button(root,text="x",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command=lambda :get_operator("*"))
+btn_mul.grid(row=3,column=3)
+
+btn_clear = Button(root,text="C",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command=clear)
+btn_clear.grid(row=4,column=0)
+
+btn_0 = Button(root,text="0",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda:get_digit(0))
+btn_0.grid(row=4,column=1)
+
+btn_equals = Button(root,text="/",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command= lambda: get_operator("/"))
+btn_equals.grid(row=4,column=2)
+
+btn_div = Button(root,text="=",width=5,height=2,font=("verdana",10),background="#00a65a",fg="white",command=get_result)
+btn_div.grid(row=4,column=3)
+
+
+
+
+
+root.mainloop()
