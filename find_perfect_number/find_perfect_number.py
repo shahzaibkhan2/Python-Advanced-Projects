6baf2edc11bf70aee822ef3cb7de7abb20b8abe8
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-07-27 06:55:24 -0700

    Find perfect number
    Find perfect number

--- find_perfect_number/find_perfect_number.py
+++ find_perfect_number/find_perfect_number.py
@@ -1,1 +1,16 @@
+def perfect(num):
+    """ A function which finds whether the given number is perfect number or not
+    Input: Give it a number 
+    Output: Given number is not a perfect number """
 
+    summ = 0
+    
+    for i in range(1, num):
+        if num%i == 0:
+            summ += i
+    if summ == num:
+        print(num, "Is a perfect number")
+    else:
+        print(num, "Is not a perfect number")
+
+perfect(5)
